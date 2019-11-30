20843415c842f0bf6985ef5a78d098aad82902db
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-11-30 19:55:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 document.querySelector('main#main').remove();
 let header = document.createElement('h1');
 header.setAttribute("id","victory");
-header.innerHTML = "";
+header.innerHTML = "Wow I Rock";
 document.body.appendChild(header);
