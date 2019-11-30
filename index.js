e7a533b4840d96b25f4697acc0f2727d8c6a621c
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-11-30 19:55:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code here!
 document.querySelector('main#main').remove();
 let header = document.createElement('h1');
-header.setAttribute("id","victory");
+header.setAttribute("id","rock");
 header.innerHTML = "Wow I Rock";
 document.body.appendChild(header);
